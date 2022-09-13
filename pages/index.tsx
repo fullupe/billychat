454e6cdb0f61f67fc3a7d13b9bd248cadf904a8f
william <william@williams-MacBook-Pro.local>
2022-09-13 13:52:39 +0100

    NEXTAUTH_SECRET

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Login from '../components/Login'
 import AppHome from "../components/AppHome"
-import { getSession,  useSession, signIn, signOut, GetSessionParams } from "next-auth/react";
+import { getSession,  useSession,  GetSessionParams } from "next-auth/react";
 
 interface props{ 
   sessionDetails:{}
@@ -20,7 +20,7 @@ const Home: NextPage = ({sessionDetails}:any) => {
   return (
     <div className="flex h-screen flex-col items-center justify-center py-2 bg-[#2f3233]">
       <Head>
-        <title>Create Next App</title>
+        <title>BillyChat</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {!Session ? (
